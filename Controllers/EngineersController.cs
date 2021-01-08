709532353b4d02832405464f4fba4be0212ce88b
Callie Stump <callie@stu.mp>
2021-01-08 18:41:16 -0500

    updates engineers controller & readme

--- Controllers/EngineersController.cs
+++ Controllers/EngineersController.cs
@@ -22,17 +22,12 @@ namespace Factory.Controllers
     }
     public ActionResult Create()
     {
-      //ViewBag.EngineerId = new SelectList(_db.Machines, "MachineId", "MachineName");
       return View();
     }
     [HttpPost]
-    public ActionResult Create(Engineer engineer) //, int MachineId
+    public ActionResult Create(Engineer engineer) 
     {
       _db.Engineers.Add(engineer);
-      // if (MachineId != 0)
-      // {
-      //   _db.EngineerMachine.Add(new EngineerMachine() { MachineId = MachineId, EngineerId = engineer.EngineerId });
-      // }
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
@@ -47,16 +42,11 @@ namespace Factory.Controllers
     public ActionResult Edit(int id)
     {
       var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id); // finds the match and assigns it to "thisEngineer"
-      //ViewBag.EngineerId = new SelectList(_db.Machines, "MachineId", "MachineName"); // transfers data from controller to view
       return View(thisEngineer);
     }
     [HttpPost]
-    public ActionResult Edit(Engineer engineer) //, int MachineId
+    public ActionResult Edit(Engineer engineer)
     {
-      // if (MachineId != 0)
-      // {
-      //   _db.EngineerMachine.Add(new EngineerMachine() { MachineId = MachineId, EngineerId = engineer.EngineerId });
-      // }
       _db.Entry(engineer).State = EntityState.Modified; // Holds all information for engineer
       _db.SaveChanges(); // sends information for engineer to database
       return RedirectToAction("Index"); // returns to the index page of engineers.
@@ -81,7 +71,7 @@ namespace Factory.Controllers
 
     public ActionResult Delete(int id)
     {
-      var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id); // finds the match and assigns it to "thisEngineer"
+      var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
       return View(thisEngineer);
     }
     [HttpPost, ActionName("Delete")]
